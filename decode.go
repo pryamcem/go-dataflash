e4b43c5dd876a9d35c1d36a4868b046c9270eafd
Stanislav Derevianko <pryamcem@gmail.com>
2025-12-28 22:39:19 +0200

    refactor: move some code a bit

--- decode.go
+++ decode.go
@@ -1,27 +1,12 @@
-package main
+package dataflash
 
 import (
 	"encoding/binary"
-	"fmt"
-	"io"
 	"math"
-	"os"
 	"strings"
 )
 
-const (
-	HEAD1 = 0xA3
-	HEAD2 = 0x95
-)
-
-type FMTMessage struct {
-	Type    uint8
-	Length  uint8
-	Name    string
-	Format  string
-	Columns string
-}
-
+// formatSizes maps format characters to their byte sizes
 var formatSizes = map[rune]int{
 	'B': 1,  // uint8
 	'b': 1,  // int8
@@ -43,123 +28,13 @@ var formatSizes = map[rune]int{
 	'Z': 64, // char[64]
 }
 
-func main() {
-	file, err := os.Open("log_29_2025-12-13-16-08-22.bin")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	schemas := make(map[uint8]*FMTMessage)
-	// Pass 1
-	for {
-		msgType, err := readMessageHeader(file)
-		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			break
-		}
-		if err != nil {
-			continue // Invalid header, skip
-		}
-
-		if msgType == 128 {
-			fmtMsg, _ := decodeFMTMessage(file)
-			schemas[fmtMsg.Type] = fmtMsg
-			fmt.Print(".")
-		} else {
-			file.Seek(86, io.SeekCurrent)
-		}
-	}
-
-	// Pass 2
-	file.Seek(0, io.SeekStart)
-
-	for {
-		msgType, err := readMessageHeader(file)
-		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			break
-		}
-		if err != nil {
-			continue
-		}
-
-		msgFormat, ok := schemas[msgType]
-		if !ok {
-			continue
-		}
-
-		body := make([]byte, msgFormat.Length-3)
-		_, err = io.ReadFull(file, body)
-		if err != nil {
-			break
-		}
-		data, _ := decodeMessageBody(body, msgFormat)
-
-		// Temp test
-		if msgFormat.Name == "GPS" {
-			fmt.Println(data)
-		}
-	}
-}
-
-func readMessageHeader(file *os.File) (uint8, error) {
-	header := make([]byte, 3)
-	_, err := io.ReadFull(file, header)
-	if err != nil {
-		return 0, err
-	}
-
-	if header[0] != HEAD1 || header[1] != HEAD2 {
-		return 0, fmt.Errorf("invalid header")
-	}
-
-	return header[2], nil // Return message type
-}
-
-func readString(file *os.File, maxLen int) (string, error) {
-	buf := make([]byte, maxLen)
-	_, err := file.Read(buf)
-	if err != nil {
-		return "", err
-	}
-	for i, b := range buf {
-		if b == 0 {
-			return string(buf[:i]), nil
-		}
-	}
-	return string(buf), nil
-}
-
-func decodeFMTMessage(file *os.File) (*FMTMessage, error) {
-	var msg FMTMessage
-	if err := binary.Read(file, binary.LittleEndian, &msg.Type); err != nil {
-		return nil, fmt.Errorf("reading FMT type: %w", err)
-	}
-	if err := binary.Read(file, binary.LittleEndian, &msg.Length); err != nil {
-		return nil, fmt.Errorf("reading FMT length: %w", err)
-	}
-
-	var err error
-	msg.Name, err = readString(file, 4)
-	if err != nil {
-		return nil, err
-	}
-	msg.Format, err = readString(file, 16)
-	if err != nil {
-		return nil, err
-	}
-	msg.Columns, err = readString(file, 64)
-	if err != nil {
-		return nil, err
-	}
-
-	return &msg, nil
-}
-
-func decodeMessageBody(body []byte, schema *FMTMessage) (map[string]any, error) {
-	data := make(map[string]any)
+// DecodeMessageBody decodes a message body according to the provided schema.
+// Returns a map of field names to their decoded values.
+func DecodeMessageBody(body []byte, schema *Schema) (map[string]interface{}, error) {
+	data := make(map[string]interface{})
 	columns := strings.Split(schema.Columns, ",")
 
-	offset := 0 // Track position in body bytes
+	offset := 0
 
 	for i, dataType := range schema.Format {
 		// Stop at null terminator
@@ -175,12 +50,12 @@ func decodeMessageBody(body []byte, schema *FMTMessage) (map[string]any, error) 
 		columnName := columns[i]
 
 		// Decode based on format type
-		var value any
+		var value interface{}
 		switch dataType {
 		// Unsigned integers
 		case 'B': // uint8
 			value = body[offset]
-		case 'H': // uing16
+		case 'H': // uint16
 			value = binary.LittleEndian.Uint16(body[offset:])
 		case 'I': // uint32
 			value = binary.LittleEndian.Uint32(body[offset:])
@@ -229,15 +104,16 @@ func decodeMessageBody(body []byte, schema *FMTMessage) (map[string]any, error) 
 			value = strings.TrimRight(string(body[offset:offset+16]), "\x00")
 		case 'Z': // char[64]
 			value = strings.TrimRight(string(body[offset:offset+64]), "\x00")
-		default: // Unknown
+
+		default: // Unknown format type
 			offset += formatSizes[dataType]
 			continue
 		}
 
-		// Store the value
+		// Store the decoded value
 		data[columnName] = value
 
-		// Move offset forward
+		// Move offset forward by the size of this field
 		offset += formatSizes[dataType]
 	}
 
